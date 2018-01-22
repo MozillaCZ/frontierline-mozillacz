058de7fd8c4ee4258c124ffd1369ca89c2d3f3e6
Michal Stanke <michal@stanke.cz>
2018-01-22 11:16:01 +0100

    Parent CSS přes enqueue, fix #1

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
+
+/* Enqueue Frontierline original stylesheet */
+add_action('wp_enqueue_scripts', 'frontierline_css');
+function frontierline_css() {
+    wp_enqueue_style('frontierline', get_template_directory_uri().'/style.css');
+}
+
 /* Enqueue Font Awesome */
 add_action('wp_enqueue_scripts', 'font_awesome_css');
 function font_awesome_css() {

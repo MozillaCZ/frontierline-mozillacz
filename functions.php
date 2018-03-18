6bea9c35585273bbf4365c9f891788965f43b089
Michal Stanke <michal@stanke.cz>
2018-03-18 11:43:44 +0100

    Výchozí obrázek v záhlaví, fix #6, fix #2

--- functions.php
+++ functions.php
@@ -12,3 +12,12 @@ function font_awesome_css() {
     wp_enqueue_style('font_awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 }
 
+/* Register Mozilla Communities logo for setting it into the header */
+register_default_headers( array(
+	'mozilla-cz' => array(
+		'url' => get_stylesheet_directory_uri().'/img/header-mozillacz-03.png',
+		'thumbnail_url' => get_stylesheet_directory_uri().'/img/header-mozillacz-03.png',
+		'description' => 'Mozilla.cz'
+	)
+ )
+);

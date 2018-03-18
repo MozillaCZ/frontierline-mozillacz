5ca7ee91e45d3c44727050b81666c419ae7c228d
Michal Stanke <michal@stanke.cz>
2018-03-18 14:51:09 +0100

    Font Awesome 5, #3

--- functions.php
+++ functions.php
@@ -7,9 +7,17 @@ function frontierline_css() {
 }
 
 /* Enqueue Font Awesome */
-add_action('wp_enqueue_scripts', 'font_awesome_css');
-function font_awesome_css() {
-    wp_enqueue_style('font_awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+add_action('wp_enqueue_scripts', 'font_awesome_js');
+function font_awesome_js() {
+    wp_enqueue_script('font_awesome', 'https://use.fontawesome.com/releases/v5.0.8/js/all.js');
+}
+
+add_filter('script_loader_tag', 'font_awesome_html_tag', 10, 3);
+function font_awesome_html_tag($tag, $handle, $src) {
+    if ('font_awesome' === $handle) {
+        $tag = str_replace('></script>', ' defer integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>', $tag);
+    }
+    return $tag;
 }
 
 /* Register Mozilla Communities logo for setting it into the header */

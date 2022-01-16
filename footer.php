a3e64d4989a6c20aede2d03a87f56c5e02caa15b
Michal Stanke <michal@stanke.cz>
2022-01-16 15:13:14 +0100

    Aktualizace odkazu na Matrix space

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
         </a>
         <ul>
           <li><a href="https://twitter.com/MozillaCZ" target="_blank">Twitter</a></li>
-          <li><a href="https://matrix.to/#/+mozillacz:mozilla.org" target="_blank">Mozilla chat / Matrix</a></li>
+          <li><a href="https://matrix.to/#/#mozillacz-space:mozilla.org" target="_blank">Mozilla chat / Matrix</a></li>
           <li><a href="https://github.com/MozillaCZ" target="_blank">GitHub</a></li>
         </ul>
       </section>
